9a3e6343bbb73efe3293f9ec87c4226cd65fa772
Mykola Mykhaliuk <Mykola.Mykhaliuk@jobframe.de>
2015-06-04 16:51:45 +0200

    one more time

--- AppHarbor/CodeTemplates.WebMVCWebApi/Startup.cs
+++ AppHarbor/CodeTemplates.WebMVCWebApi/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(CodeTemplates.WebMVCWebApi.Startup))]
@@ -13,6 +10,7 @@ namespace CodeTemplates.WebMVCWebApi
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
         }
     }
 }
