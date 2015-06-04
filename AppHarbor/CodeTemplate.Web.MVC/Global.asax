3112b24b998a3139fbd48bcfe33a68419c362bd5
Mykola Mykhaliuk <Mykola.Mykhaliuk@jobframe.de>
2015-06-04 16:57:10 +0200

    test new MVC

---
+++ AppHarbor/CodeTemplate.Web.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeTemplate.Web.MVC.MvcApplication" Language="C#" %>
---
+++ AppHarbor/CodeTemplate.Web.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeTemplate.Web.MVC.Startup))]
+namespace CodeTemplate.Web.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
