5f9817526a68fa769b5e2d45c9c04ad9396cb72a
Mykola Mykhaliuk <Mykola.Mykhaliuk@jobframe.de>
2015-06-04 16:38:54 +0200

    Add ionic, add patterns

---
+++ AppHarbor/CodeTemplates.CodePatterns/DesignPatterns/Creational/Singletone.cs
@@ -0,0 +1,31 @@
+﻿
+namespace CodeTemplates.CodePatterns.DesignPatterns.Creational
+{
+	/// <summary>
+	/// Simple what it does:
+	///  
+	/// Where it used:
+	/// 
+	/// </summary>
+	public class Singleton
+	{
+		private static Singleton _instance;
+ 
+		// Constructor is 'protected'
+		protected Singleton()
+		{
+		}
+ 
+		public static Singleton Instance()
+		{
+			// Uses lazy initialization.
+			// Note: this is not thread safe.
+			if (_instance == null)
+			{
+				_instance = new Singleton();
+			}
+ 
+			return _instance;
+		}
+	}
+}
